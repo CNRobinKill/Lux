6d78e59c7ed5cff6a96e9a068243037b8ebddc9f
Finkle <sikong1986@gmail.com>
2013-04-07 11:17:57 +0800

    base code

---
+++ ERP.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// add new references
+using System.Web.Routing;
+using System.Web.Security;
+using ERP.WebUI.Models;
+
+namespace ERP.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {       
+        // GET: /Account/LogOn
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        // POST: /Account/LogOn
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            //return View(model);
+
+            // This is a test
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
